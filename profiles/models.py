df6be686de067fa75525863e5fd2cbfe02d493b4
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-13 16:00:17 +0000

    Add profile model fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    website = models.URLField(max_length=200, blank=True)
+    instagram = models.URLField(max_length=200, blank=True)
+    facebook = models.URLField(max_length=200, blank=True)
+    phone = models.CharField(max_length=20)
+    profile_pic = models.ImageField(
+        upload_to='images/', default='../profile-pic_nfhakf'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
