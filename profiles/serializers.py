4f5f15b8774d2434d0d990161665d1ee60908e0b
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-15 15:28:55 +0000

    feat: Add custom IsOwner permission class and add it to profile serializer #3

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,12 +4,17 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'bio', 'website','instagram_link', 'facebook_link',
-            'phone_number', 'profile_pic'
+            'bio', 'website', 'instagram_link', 'facebook_link',
+            'phone_number', 'profile_pic', 'is_owner'
 
         ]
