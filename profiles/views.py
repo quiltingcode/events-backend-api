c664caeb585d0e421aa50a2b540c68f534c8a7f8
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-15 14:19:30 +0000

    feat: Add edit profile view to complete full profile fields #3

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -13,6 +14,7 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -24,3 +26,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
