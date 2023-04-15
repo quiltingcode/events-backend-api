d0e027cd3e052f56a0e882019f5d7b0d8a6b84a8
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-15 14:05:52 +0000

    feat: add profile detail view and url path #2

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -8,4 +9,18 @@ class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
