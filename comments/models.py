596ec0d63f7ecb8cdba818b240ea8bd19805c64f
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 13:32:59 +0000

    feat: Add comment model and migrate to db #11

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
