8587a19e9def4b91b9ebfc3add1c5df7a6206b92
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 10:06:20 +0000

    feat: Add image validation to event model and serializer

--- events/models.py
+++ events/models.py
@@ -12,6 +12,17 @@ EVENT_CATEGORIES = (
 
 
 class Event(models.Model):
+
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -19,6 +30,9 @@ class Event(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_giz2az', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
     event_date = models.DateField(blank=True)
     tags = models.CharField(max_length=100)
--- events/serializers.py
+++ events/serializers.py
@@ -12,11 +12,26 @@ class EventSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
+
     class Meta:
         model = Event
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'title', 'description', 'image', 'event_date',
             'tags', 'category', 'is_owner', 'profile_id',
-            'profile_image'
+            'profile_image', 'image_filter'
         ]
