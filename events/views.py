404b0ad8fed24ab1c7a751c8b30d0c528efaa356
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 11:28:06 +0000

    feat: Add view for event detail and edit an event #7 #6

--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Event
 from .serializers import EventSerializer
+from events_api.permissions import IsOwnerOrReadOnly
 
 
 class EventList(APIView):
@@ -30,3 +32,33 @@ class EventList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class EventDetail(APIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            event = Event.objects.get(pk=pk)
+            self.check_object_permissions(self.request, event)
+            return event
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(
+            event, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(
+            event, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
