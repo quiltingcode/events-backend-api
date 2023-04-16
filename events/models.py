ccc81e48e12779f160fb884ae71a82140bebc54a
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 10:59:20 +0000

    feat: Add create event form and revise model field validation attributes #10

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 EVENT_CATEGORIES = (
     ("Sport", "Sport"),
@@ -14,13 +15,13 @@ EVENT_CATEGORIES = (
 class Event(models.Model):
 
     image_filter_choices = [
-    ('_1977', '1977'), ('brannan', 'Brannan'),
-    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
-    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
-    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
-    ('nashville', 'Nashville'), ('rise', 'Rise'),
-    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
-    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+        ('_1977', '1977'), ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+        ('nashville', 'Nashville'), ('rise', 'Rise'),
+        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+        ('walden', 'Walden'), ('xpro2', 'X-pro II')
     ]
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,16 +33,16 @@ class Event(models.Model):
         upload_to='images/', default='../default_post_giz2az', blank=True
     )
     image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
+        max_length=32, choices=image_filter_choices, default='Normal'
     )
-    event_date = models.DateField(blank=True)
-    tags = models.CharField(max_length=100)
+    event_date = models.DateField(default=date.today)
+    tags = models.CharField(max_length=100, blank=True)
     category = models.CharField(
         max_length=50, choices=EVENT_CATEGORIES, default='Culture'
     )
 
     class Meta:
         ordering = ['-created_at']
-        
+
     def __str__(self):
         return f'{self.id} {self.title}'
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Event
@@ -6,9 +6,27 @@ from .serializers import EventSerializer
 
 
 class EventList(APIView):
+    serializer_class = EventSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         events = Event.objects.all()
         serializer = EventSerializer(
             events, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EventSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
