f16782dac12aa8df5e879583d87bb3e1ffe3cadb
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 10:33:19 +0000

    feat: Create views and url path to view events list #5

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventList(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(
+            events, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
