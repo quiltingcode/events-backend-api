f041bb8338150506ce52f13e19e6cc9046a45f8f
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 13:47:36 +0000

    feat: Add comment serializer #11

---
+++ comments/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'event',
+            'content', 'is_owner', 'profile_id', 'profile_image',
+        ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    event = serializers.ReadOnlyField(source='event.id')
