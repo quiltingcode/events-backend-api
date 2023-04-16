e327bf603677a32860a5aa248ee9a63b35679b6e
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 17:53:17 +0000

    feat: Add duplicate validate to interested serializer #17

--- interested/serializers.py
+++ interested/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Interested
+from django.db import IntegrityError
 
 
 class InterestedSerializer(serializers.ModelSerializer):
@@ -10,3 +11,11 @@ class InterestedSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'event',
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You are already interested!'
+            })
