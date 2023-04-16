49c3892ac9da1300446ced1dcd63c1b193f4cf0d
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-16 14:21:32 +0000

    feat: Add comment detail view to retrieve a single comment and edit and delete it #13 #14 #15

--- comments/urls.py
+++ comments/urls.py
@@ -3,4 +3,5 @@ from comments import views
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
--- comments/views.py
+++ comments/views.py
@@ -13,3 +13,7 @@ class CommentList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
