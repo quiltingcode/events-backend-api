f809812a7b36e26e5900ddf99e1220ec0923d10a
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-17 13:26:08 +0000

    feat: Create follows app and add model fields #22

--- events_api/settings.py
+++ events_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'comments',
     'interested',
     'going',
+    'followers',
 ]
 
 MIDDLEWARE = [
---
+++ followers/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Followers(models.Model):
+    """
+    Follower model, related to 'owner' and 'followed'.
+    'owner' is a User that is following a User.
+    'followed' is a User that is followed by 'owner'.
+    """
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following'
+    )
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
