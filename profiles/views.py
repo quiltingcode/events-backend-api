549051dd6d053e2c7df32354982d52fe7848357b
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-19 08:40:00 +0000

    feat: Add events followers and following count to profile view #31

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from events_api.permissions import IsOwnerOrReadOnly
@@ -11,7 +12,11 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        post_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
