6b126557f992f1c779beee5dabf17a2ba418cfb5
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-19 13:36:14 +0000

    fix: add related name to event FK field

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,9 @@ from events.models import Event
 
 class Review(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, related_name='reviews', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     review = models.TextField()
