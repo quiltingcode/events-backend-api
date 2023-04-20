5b934715b8da34ac5da87b54b7f361a7667b02c1
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-20 14:27:24 +0000

    feat: Add pagination to settings

--- events_api/settings.py
+++ events_api/settings.py
@@ -32,7 +32,14 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
