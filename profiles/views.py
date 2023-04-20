fb7b67b63e72e9b0048e3f27161fe2b6c2a0cd20
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-20 13:19:04 +0000

    feat: Add filterset field for profiles followed by a profile #44

--- profiles/views.py
+++ profiles/views.py
@@ -23,7 +23,8 @@ class ProfileList(generics.ListAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__following__followed__profile'
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'events_count',
