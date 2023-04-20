8a7c08d882d477129670ef1780584495bea55eb0
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-20 13:31:40 +0000

    feat: add filter to comments view to filter by event #45

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,15 @@ from rest_framework import generics, permissions
 from events_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
