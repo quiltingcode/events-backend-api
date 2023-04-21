e8fd64995c60c7b0f6f156281d1c15e50de2e1ac
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-21 15:00:49 +0000

    Add interested app test setup

--- events/tests.py
+++ events/tests.py
@@ -8,25 +8,28 @@ class EventListViewTests(APITestCase):
     def setUp(self):
         User.objects.create_user(username='kelly', password='letmein')
 
-    def test_can_list_posts(self):
+    def test_can_list_events(self):
         kelly = User.objects.get(username='kelly')
         Event.objects.create(owner=kelly, title='event title')
         response = self.client.get('/events/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_logged_in_user_can_create_post(self):
+    def test_logged_in_user_can_create_event(self):
         self.client.login(username='kelly', password='letmein')
         response = self.client.post('/events/', {'title': 'event title'})
         count = Event.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_logged_out_user_cant_create_post(self):
+    def test_logged_out_user_cant_create_event(self):
         response = self.client.post('/events/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        count = Event.objects.count()
+        self.assertEqual(count, 0)
 
 
 class EventDetailViewTests(APITestCase):
+
     def setUp(self):
         kelly = User.objects.create_user(username='kelly', password='letmein')
         greg = User.objects.create_user(username='greg', password='pass')
@@ -43,7 +46,7 @@ class EventDetailViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_cant_retrieve_post_using_invalid_id(self):
-        response = self.client.get('/posts/999/')
+        response = self.client.get('/events/999/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_user_can_update_own_post(self):
--- interested/tests.py
+++ interested/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Event
+from .models import Interested
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class InterestedListViewTests(APITestCase):
+    def setUp(self):
+        kelly = User.objects.create_user(username='kelly', password='letmein')
+        event_a = Event.objects.create(owner=kelly, title='a fun event')
+
+    def test_can_list_interested(self):
+        kelly = User.objects.get(username='kelly')
+        event_a = Event.objects.get(id=1)
+        Interested.objects.create(owner=kelly, event=event_a)
+        response = self.client.get('/interested/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
