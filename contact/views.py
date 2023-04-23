7d84df15807c287619df65bbdbb8004608f296b1
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-23 12:23:36 +0000

    fix: stop logged out users from creating contacts

--- contact/views.py
+++ contact/views.py
@@ -9,7 +9,7 @@ class ContactList(generics.ListCreateAPIView):
     List contacts or create a contact if logged in.
     """
     serializer_class = ContactSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Contact.objects.all()
 
     def perform_create(self, serializer):
@@ -20,6 +20,6 @@ class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a contact, or update or delete it by id.
     """
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ContactDetailSerializer
     queryset = Contact.objects.all()
