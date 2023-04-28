571a18f0539da029dc306bac55ca604b9006c472
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-28 10:58:51 +0000

    fix: change profile image source in user serializer

--- events_api/serializers.py
+++ events_api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(
-        source='owner.profile.profile_pic.url'
+        source='profile.profile_pic.url'
     )
 
     class Meta(UserDetailsSerializer.Meta):
